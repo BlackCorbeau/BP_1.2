0450d4d439e66460585e62ad5dd4b125d13650ed
Remizov_Kirill <woron0987654321@gmail.com>
2024-03-05 17:28:40 +0300

    First version

---
+++ Date_time_project/Date_time_project/class.h
@@ -0,0 +1,17 @@
+#pragma once
+class CTime
+{
+private:
+	int second, minute, hour;
+public:
+	CTime();
+	CTime(int _second, int _minute, int _hour);
+	~CTime();
+	void initialize_seconds(int _second);
+	void initialize_minutes(int _minutes);
+	void initialize_hours(int _hours);
+	int get_seconds();
+	int get_minutes();
+	int get_hours();
+	void print_parametrs();
+};
---
+++ Date_time_project/Date_time_project/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "class.h"
+
+int main()
+{
+	CTime t1(12, 43, 24);
+	t1.print_parametrs();
+	return 0;
+}
